d9359e9dd41fecea0e41c36a19616fa07c4e85b5
MMILHA <98252587+MossabMilha@users.noreply.github.com>
2025-09-28 18:09:18 +0100

    Fix Api Problem

--- cfa-conseil/routes/api.php
+++ cfa-conseil/routes/api.php
@@ -1,6 +1,12 @@
 <?php
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/ping', function () {
+    return response()->json([
+        'message' => 'API is working in Laravel 12!'
+    ]);
+});
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout']);
