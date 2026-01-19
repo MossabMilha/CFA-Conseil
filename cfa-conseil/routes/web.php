3185bd6d04dab42c2c7c3e430af3528a8d98b8d3
Elafifiyoussef <elafifi.yo@gmail.com>
2026-01-19 14:15:12 +0100

    Mark routes as reviewed by adding "checked" comments for clarity and future maintainability.

--- cfa-conseil/routes/web.php
+++ cfa-conseil/routes/web.php
@@ -11,17 +11,17 @@ use Inertia\Inertia;
 // --------------------------
 // Public Pages
 // --------------------------
-Route::get('/', fn() => Inertia::render('Home'))->name('home');
-Route::get('/blogs', [BlogController::class, 'index'])->name('blogs.index');
-Route::get('/blog/{slug}', [BlogController::class, 'show'])->name('blogs.show');
-Route::get('/contact', fn() => Inertia::render('Contact'))->name('contact');
-Route::get('/services/{service}', [ServiceController::class, 'show'])->name('services.show');
+Route::get('/', fn() => Inertia::render('Home'))->name('home'); // checked
+Route::get('/blogs', [BlogController::class, 'index'])->name('blogs.index'); // checked
+Route::get('/blog/{slug}', [BlogController::class, 'show'])->name('blogs.show'); // checked
+Route::get('/contact', fn() => Inertia::render('Contact'))->name('contact'); // checked
+Route::get('/services/{service}', [ServiceController::class, 'show'])->name('services.show'); // checked
 
 // --------------------------
 // Public Blog Comments
 // --------------------------
-Route::get('/blogs/{blog}/comments', [CommentController::class, 'index'])->name('comments.index');
-Route::post('/blogs/{blog}/comments', [CommentController::class, 'store'])->name('comments.store');
+Route::get('/blogs/{blog}/comments', [CommentController::class, 'index'])->name('comments.index'); // checked
+Route::post('/blogs/{blog}/comments', [CommentController::class, 'store'])->name('comments.store'); // checked
 
 // --------------------------
 // Authentication
@@ -39,20 +39,20 @@ Route::middleware(['auth', 'role'])->group(function () {
 
     // Blog Management
     Route::post('/blogs', [BlogController::class, 'store'])->name('blogs.store'); // checked
-    Route::post('/blogs/{slug}', [BlogController::class, 'update'])->name('blogs.update');
-    Route::delete('/blogs/{id}', [BlogController::class, 'destroy'])->name('blogs.destroy');
-    Route::post('/blogs/content/{slug}', [BlogController::class, 'updateContent'])->name('blogs.updateContent');
-    Route::post('/upload-images', [BlogController::class, 'uploadImages'])->name('blogs.uploadImages');
-    Route::post('/upload-pdfs', [BlogController::class, 'uploadPdfs'])->name('blogs.uploadPdfs');
+    Route::post('/blogs/{slug}', [BlogController::class, 'update'])->name('blogs.update'); // checked
+    Route::delete('/blogs/{id}', [BlogController::class, 'destroy'])->name('blogs.destroy'); // checked
+    Route::post('/blogs/content/{slug}', [BlogController::class, 'updateContent'])->name('blogs.updateContent'); // checked
+    Route::post('/upload-images', [BlogController::class, 'uploadImages'])->name('blogs.uploadImages'); // checked
+    Route::post('/upload-pdfs', [BlogController::class, 'uploadPdfs'])->name('blogs.uploadPdfs'); // checked
 
     // Blog Editor Pages
     Route::get('/blog-editor', fn() => Inertia::render('BlogEditor'))->name('blog.editor'); // checked
-    Route::get('/blog-editor/{slug}', [BlogController::class, 'edit'])->name('blog.editor.edit');
+    Route::get('/blog-editor/{slug}', [BlogController::class, 'edit'])->name('blog.editor.edit'); // checked
 
     // Admin Comments Management
     Route::get('/admin/comments', [CommentController::class, 'adminPending'])->name('admin.comments');   // checked
 
     // Comment Actions (Approve/Delete)
-    Route::post('/comments/{comment}/approve', [CommentController::class, 'approve'])->name('comments.approve');
-    Route::delete('/comments/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy');
+    Route::post('/comments/{comment}/approve', [CommentController::class, 'approve'])->name('comments.approve'); // checked
+    Route::delete('/comments/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy'); // checked
 });
